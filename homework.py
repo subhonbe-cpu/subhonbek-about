4d05da45aead89f5db3382dc8c3ca14eed625070
Subhonbe Marufov <subhonbe@gmail.com>
2026-07-22 14:56:06 +0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,45 @@
+# # 1
+# talabalar = {"Ali","Suhrob","Umar","Sobir","Bobur","Johon"}
+# print(f"Talabalar toplami:{talabalar}")
+# talabalar.add(input("ismingizni kiriting:").title().strip())
+# print(talabalar)
+
+# # 2
+# talabalar = {"Ali","Suhrob","Umar","Sobir","Bobur","Johon"}
+# print(talabalar)
+# talabalar.pop()
+# print(talabalar)
+
+# # 3
+# talabalar = {"Ali","Suhrob","Umar","Hasan","Sobir","Bobur","Johon"}
+# print(talabalar)
+# talabalar.remove("Hasan")
+# print(talabalar)
+
+# # 4
+# talabalar = {"Ali","Suhrob","Umar"}
+# talaba = {"Sobir","Bobur","Johon"}
+# print(talabalar)
+# print(talaba)
+# umuiy_talabalar = talabalar.union(talaba)
+# print(umuiy_talabalar)
+
+# # 5
+# talabalar = {"Ali","Suhrob","Umar"}
+# keyingi_talabalar = {"Sobir","Bobur","Johon"}
+# hammasi = talabalar.update(keyingi_talabalar)
+# print(hammasi)
+
+# # 6
+# """
+# qilishni bilmadim nima qilini bilmadim 
+# """
+# # 7
+# talabalar = {"Ali","Suhrob","Umar","Sobir","Bobur","Johon"}
+# print(talabalar)
+# talabalar.clear()
+# print(talabalar) 
+
+
+
+
